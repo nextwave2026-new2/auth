aab7669ad82551026cc07c8ca01e4229c1bdc83c
DucPham28-dev <dpham@nwemon.com>
2026-01-14 10:36:00 +0700

    Refactor CI/CD workflow and Dockerfile for improved build process

--- src/main/java/com/nextwave/auth/controller/AuthController.java
+++ src/main/java/com/nextwave/auth/controller/AuthController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
     @GetMapping("/")
     public String hello() {
-        return "Hello auth service!tui là duc hand some vip pro no 1";
+        return "Hello auth service! Tui là Duc handsome - Auto deploy working!";
     }
 }
